eb3fcd877e86cc2357351557a2bfc77d527be952
Massimiliano Gubinelli <m.gubinelli@gmail.com>
2016-07-22 13:48:12 +0200

    Move file related buffer info class into abs_buffer header

--- src/src/Edit/abs_buffer.hpp
+++ src/src/Edit/abs_buffer.hpp
@@ -9,13 +9,52 @@
 #ifndef ABS_BUFFER_HPP
 #define ABS_BUFFER_HPP
 
+#include "new_document.hpp"
 #include "new_data.hpp"
-#include "../Texmacs/Data/new_buffer.hpp"
 
+/******************************************************************************
+ * file related information about buffers
+ ******************************************************************************/
 
-extern tree the_et;
-path new_document ();
+class new_buffer;
+class new_buffer_rep: public concrete_struct {
+public:
+  url name;               // full name
+  url master;             // base name for linking and navigation
+  string fm;              // buffer format
+  string title;           // buffer title (for menus)
+  bool read_only;         // buffer is read only?
+  bool secure;            // is the buffer secure?
+  int last_save;          // last time that the buffer was saved
+  time_t last_visit;      // time that the buffer was visited last
+  
+  inline new_buffer_rep (url name2):
+  name (name2), master (name2),
+  fm ("texmacs"), title (as_string (tail (name))),
+  read_only (false), secure (is_secure (name2)),
+  last_save (- (int) (((unsigned int) (-1)) >> 1)),
+  last_visit (texmacs_time ()) {}
+};
 
+class new_buffer;
+class new_buffer {
+  CONCRETE(new_buffer);
+  inline new_buffer (url name): rep (tm_new<new_buffer_rep> (name)) {}
+};
+//CONCRETE_CODE(new_buffer);
+
+inline new_buffer::new_buffer (const new_buffer& x):
+rep(x.rep) { INC_COUNT (this->rep); }
+inline new_buffer::~new_buffer () { DEC_COUNT (this->rep); }
+inline new_buffer_rep* new_buffer::operator -> () {
+  return rep; }
+inline new_buffer& new_buffer::operator = (new_buffer x) {
+  INC_COUNT (x.rep); DEC_COUNT (this->rep);
+  this->rep=x.rep; return *this; }
+
+/******************************************************************************
+ * The abstract buffer class
+ ******************************************************************************/
 
 class abs_buffer_rep;
 typedef abs_buffer_rep *abs_buffer;
