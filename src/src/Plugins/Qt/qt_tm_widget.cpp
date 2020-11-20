f32bef58d1b205314d87015aa43eb1fa8c0e31d5
Massimiliano Gubinelli <m.gubinelli@gmail.com>
2020-11-20 23:17:55 +0100

    Fix support for retina icons

--- src/src/Plugins/Qt/qt_tm_widget.cpp
+++ src/src/Plugins/Qt/qt_tm_widget.cpp
@@ -203,17 +203,32 @@ qt_tm_widget_rep::qt_tm_widget_rep(int mask, command _quit)
   bottomTools->setStyle (qtmstyle ());
   
   {
+#if (QT_VERSION >= 0x050000)
+     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+ #endif
+
+    float dpr = 1.0;
     // set proper sizes for icons
     QImage *pxm = xpm_image ("tm_new.xpm");
-    QSize sz = (pxm ? pxm->size() : QSize (24, 24));
+#if (QT_VERSION >= 0x050000)
+    dpr = pxm->devicePixelRatio();
+#endif
+    QSize sz = (pxm ? pxm->size()/dpr : QSize (24, 24));
     tweak_iconbar_size (sz);
     mainToolBar->setIconSize (sz);
     pxm = xpm_image ("tm_section.xpm");
-    sz = (pxm ? pxm->size() : QSize (20, 20));
+    sz = (pxm ? pxm->size()/dpr : QSize (20, 20));
+#if (QT_VERSION >= 0x050000)
+      dpr = pxm->devicePixelRatio();
+#endif
     tweak_iconbar_size (sz);
     modeToolBar->setIconSize (sz);
     pxm = xpm_image ("tm_add.xpm");
-    sz = (pxm ? pxm->size() : QSize (16, 16));
+#if (QT_VERSION >= 0x050000)
+      dpr = pxm->devicePixelRatio();
+#endif
+    sz = (pxm ? pxm->size()/dpr : QSize (16, 16));
     tweak_iconbar_size (sz);
     focusToolBar->setIconSize (sz);
   }
