713232ef09e24d94e400ef63916ebf9fe61e2cdb
mgubi <m.gubinelli@gmail.com>
2025-09-11 00:31:29 +0100

    Starting work on scrollbars

--- src/src/Plugins/Vue/vue_widget.cpp
+++ src/src/Plugins/Vue/vue_widget.cpp
@@ -723,6 +723,61 @@ typedef struct
 ScrollbarData scrollbarData= { {0, 0}, {0, 0}, false };
 
 void
+scroll_bar (unsigned int id, Clay_ScrollContainerData &scrollData, Clay_ElementId &my_id, Clay_ElementData &canvas_layout) {
+  Clay_ElementId sb_id= CLAY_IDI("ScrollBarV", id);
+  CLAY({
+    .id= sb_id,
+    .floating= {
+      .attachTo= CLAY_ATTACH_TO_ELEMENT_WITH_ID,
+        .offset= { .y= -(scrollData.scrollPosition->y / scrollData.contentDimensions.height) * scrollData.scrollContainerDimensions.height },
+        .zIndex= 1,
+        .parentId= my_id.id,
+        .attachPoints= {
+          .element= CLAY_ATTACH_POINT_RIGHT_TOP,
+          .parent=  CLAY_ATTACH_POINT_RIGHT_TOP }}})
+  {
+    CLAY({
+      .id= CLAY_IDI("ScrollBarButtonV", id),
+      .layout= {
+        .sizing= {
+           CLAY_SIZING_FIXED(24),
+           CLAY_SIZING_FIXED((scrollData.scrollContainerDimensions.height / scrollData.contentDimensions.height) * scrollData.scrollContainerDimensions.height) }},
+      .backgroundColor= Clay_PointerOver (sb_id)
+            ? (Clay_Color){100, 100, 140, 150}
+            : (Clay_Color){120, 120, 160, 150} ,
+      .cornerRadius= CLAY_CORNER_RADIUS(12) }) {}
+  }
+  //FIXME: mouse handling still not ok
+  if (!(mouse_state & 1)) {
+      scrollbarData.mouseDown= false;
+  }
+  if (mouse_action == "press-left" && !scrollbarData.mouseDown && Clay_PointerOver (sb_id)) {
+    mouse_action= "";
+    scrollbarData.clickOrigin= { (float) mouse_x, (float) mouse_y };
+    scrollbarData.positionOrigin= *scrollData.scrollPosition;
+    scrollbarData.mouseDown= true;
+  } else if (scrollbarData.mouseDown) {
+    if (scrollData.contentDimensions.height > 0) {
+      Clay_Vector2 ratio= (Clay_Vector2) {
+        scrollData.contentDimensions.width / scrollData.scrollContainerDimensions.width,
+        scrollData.contentDimensions.height / scrollData.scrollContainerDimensions.height,
+      };
+      if (scrollData.config.vertical) {
+        scrollData.scrollPosition->y= scrollbarData.positionOrigin.y + (scrollbarData.clickOrigin.y - mouse_y) * ratio.y;
+        scrollData.scrollPosition->y= min ( max (scrollData.scrollPosition->y, -(max(scrollData.contentDimensions.height - canvas_layout.boundingBox.height, 0.0f))), 0.0f);
+      }
+      if (scrollData.config.horizontal) {
+        scrollData.scrollPosition->x= scrollbarData.positionOrigin.x + (scrollbarData.clickOrigin.x - mouse_x) * ratio.x;
+        scrollData.scrollPosition->x= min ( max (scrollData.scrollPosition->x, -(max(scrollData.contentDimensions.width - canvas_layout.boundingBox.width, 0.0f))), 0.0f);
+      }
+    }
+  }
+
+}
+
+
+
+void
 vue_ui_rep::do_layout () {
   if (type == "horizontal_menu") {
     vue_horizontal_menu d= open_box<vue_horizontal_menu> (data);
@@ -1228,54 +1283,7 @@ vue_ui_rep::do_layout () {
     Clay_ScrollContainerData scrollData= Clay_GetScrollContainerData (my_id);
     Clay_ElementData canvas_layout= Clay_GetElementData (my_id);
     if (scrollData.found && canvas_layout.found) {
-      Clay_ElementId sb_id= CLAY_IDI("ScrollBar", id);
-      CLAY({
-        .id= sb_id,
-        .floating= {
-          .attachTo= CLAY_ATTACH_TO_ELEMENT_WITH_ID,
-            .offset= { .y= -(scrollData.scrollPosition->y / scrollData.contentDimensions.height) * scrollData.scrollContainerDimensions.height },
-            .zIndex= 1,
-            .parentId= my_id.id,
-            .attachPoints= {
-              .element= CLAY_ATTACH_POINT_RIGHT_TOP,
-              .parent=  CLAY_ATTACH_POINT_RIGHT_TOP }}})
-      {
-        CLAY({
-          .id= CLAY_IDI("ScrollBarButton", id),
-          .layout= {
-            .sizing= {
-               CLAY_SIZING_FIXED(12),
-               CLAY_SIZING_FIXED((scrollData.scrollContainerDimensions.height / scrollData.contentDimensions.height) * scrollData.scrollContainerDimensions.height) }},
-          .backgroundColor= Clay_PointerOver (sb_id)
-                ? (Clay_Color){100, 100, 140, 150}
-                : (Clay_Color){120, 120, 160, 150} ,
-          .cornerRadius= CLAY_CORNER_RADIUS(6) }) {}
-      }
-      //FIXME: mouse handling still not ok
-      if (!(mouse_state & 1)) {
-          scrollbarData.mouseDown= false;
-      }
-      if (mouse_action == "press-left" && !scrollbarData.mouseDown && Clay_PointerOver (sb_id)) {
-        mouse_action= "";
-        scrollbarData.clickOrigin= { (float) mouse_x, (float) mouse_y };
-        scrollbarData.positionOrigin= *scrollData.scrollPosition;
-        scrollbarData.mouseDown= true;
-      } else if (scrollbarData.mouseDown) {
-        if (scrollData.contentDimensions.height > 0) {
-          Clay_Vector2 ratio= (Clay_Vector2) {
-            scrollData.contentDimensions.width / scrollData.scrollContainerDimensions.width,
-            scrollData.contentDimensions.height / scrollData.scrollContainerDimensions.height,
-          };
-          if (scrollData.config.vertical) {
-            scrollData.scrollPosition->y= scrollbarData.positionOrigin.y + (scrollbarData.clickOrigin.y - mouse_y) * ratio.y;
-            scrollData.scrollPosition->y= min ( max (scrollData.scrollPosition->y, -(max(scrollData.contentDimensions.height - canvas_layout.boundingBox.height, 0.0f))), 0.0f);
-          }
-          if (scrollData.config.horizontal) {
-            scrollData.scrollPosition->x= scrollbarData.positionOrigin.x + (scrollbarData.clickOrigin.x - mouse_x) * ratio.x;
-            scrollData.scrollPosition->x= min ( max (scrollData.scrollPosition->x, -(max(scrollData.contentDimensions.width - canvas_layout.boundingBox.width, 0.0f))), 0.0f);
-          }
-        }
-      }
+      scroll_bar (id, scrollData, my_id, canvas_layout);
     }
     return;
   }
@@ -2595,7 +2603,31 @@ vue_simple_widget_rep::do_layout () {
     .custom= { .customData= vue_render_widget },
     .userData= this })
   {
-    if (0) { // debug view
+    Clay_ElementData canvas_layout= Clay_GetElementData (clay_id);
+    Clay_Vector2 scrollPosition = {
+      .x= (float)backing_pos.x1 / ren->pixel,
+      .y= (float)backing_pos.x2 / ren->pixel };
+    Clay_ScrollContainerData scrollData= {
+      .scrollPosition= &scrollPosition,
+      .scrollContainerDimensions= {
+        .width=  canvas_layout.boundingBox.width,
+        .height= canvas_layout.boundingBox.height },
+      .contentDimensions= {
+        .width=  ((float)extents->x2 - extents->x1)/ren->pixel,
+        .height= ((float)extents->y2 - extents->y1)/ren->pixel, },
+      .config= {
+        .horizontal= true,
+        .vertical= true,
+        .childOffset= scrollPosition },
+      .found= true
+    };
+    if (scrollData.found && canvas_layout.found) {
+      scroll_bar (id, scrollData, clay_id, canvas_layout);
+      scroll_pos.x1= scrollPosition.x * ren->pixel;
+      scroll_pos.x2= scrollPosition.y * ren->pixel;
+      absolute_scroll= false;
+    }
+    if (1) { // debug view
       CLAY({
         .backgroundColor= { 80, 80, 80, 80 },
         .layout= { .padding= { 18, 18, 18, 18 } },
@@ -2606,7 +2638,10 @@ vue_simple_widget_rep::do_layout () {
         debug_text= "";
         tm_ostream out= string_ostream (debug_text);
         out << " extents:  " << extents << LF;
-        out << " viewport: " << rectangle(backing_pos.x1, backing_pos.x2);
+        out << " viewport: " << rectangle(backing_pos.x1,
+                                          backing_pos.x2 - canvas_layout.boundingBox.height * ren->pixel,
+                                          backing_pos.x1 + canvas_layout.boundingBox.width * ren->pixel,
+                                          backing_pos.x2);
         out.flush ();
         CLAY_TEXT(CLAY_TM_STRING(debug_text),
                   CLAY_TEXT_CONFIG({ .fontSize= 30, .textColor= { 0, 0, 200, 255} }));
