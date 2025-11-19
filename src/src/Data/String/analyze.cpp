mgubi/texmacs

commit 202255c334932bb293b9dde6d84aaa6beef4cbc4

Escape backward slash for tmfs, fix the xypic demo of Help->Plug-ins->Graph
git-svn-id: svn+ssh://svn.sv.gnu.org/texmacs/trunk@12172 64cb5145-927a-446d-8aed-2fb7b4773692

src/src/Data/String/analyze.cpp
@@ -929,7 +929,11 @@ unescape_guile (string s) {
   string r;
   for (i=0; i<n; i++) {
     if (s[i] == '\\') {
-      if (i+3 < n && s[i+1] == 'x'
+      if (i+1 < n && s[i+1] == '\\') {
+        r << "\\\\\\\\";
+        i+=1;
+      }
+      else if (i+3 < n && s[i+1] == 'x'
           && is_hex_digit (s[i+2]) && is_hex_digit (s[i+3])) {
         string e= s(i+2, i+4);
         r << (unsigned char) from_hexadecimal (e);

src/TeXmacs/examples/texts/tmfs-test.tm
@@ -1,0 +1,43 @@
+<TeXmacs|1.99.11>
+
+<style|generic>
+
+<\body>
+  <\session|scheme|default>
+    <\unfolded-io|Scheme] >
+      (tmfs-load-handler (test what)
+
+      \ \ `(document
+
+      \ \ \ \ \ (TeXmacs ,(texmacs-version))
+
+      \ \ \ \ \ (style (tuple "generic"))
+
+      \ \ \ \ \ (body (document "\\\\"))))
+    <|unfolded-io>
+      #\<less\>procedure #f (what)\<gtr\>
+    </unfolded-io>
+
+    <\folded-io|Scheme] >
+      (load-document "tmfs://test/1")
+    <|folded-io>
+      \;
+    </folded-io>
+
+    <\unfolded-io|Scheme] >
+      "\\\\"
+    <|unfolded-io>
+      "\\\\"
+    </unfolded-io>
+
+    <\input|Scheme] >
+      \;
+    </input>
+  </session>
+
+  The <scm|load-document> action should open a new <TeXmacs> buffer with two
+  backward slashes.
+</body>
+
+<initial|<\collection>
+</collection>>

src/tests/Data/String/analyze_test.cpp
@@ -76,6 +76,10 @@ TEST (string, raw_unquote) {
   ASSERT_STREQ (as_charp (raw_unquote ("a")), "a");
 }
 
+TEST (string, unescape_guile) {
+  ASSERT_STREQ (as_charp (unescape_guile ("\\\\")), "\\\\\\\\");
+}
+
 TEST (string, starts) {
   ASSERT_TRUE (starts ("abc_def", "abc"));
   ASSERT_FALSE (starts ("abc_def", "def"));

commit 2bcd5a185947060591d4b9684e543b4c398eb69f

This patch prevents texmacs from adding extra blackslashes in session commands containing strings when treated via tmfs in the stm-document format (e.g. parts of books, remote files). This issue was introduced in r12172 to fix the xypic demo of Help->Plug-ins->Graph. But reverting commit r12172 does not seem to break this demo.
git-svn-id: svn+ssh://svn.sv.gnu.org/texmacs/trunk@15124 64cb5145-927a-446d-8aed-2fb7b4773692

src/src/Data/String/analyze.cpp
@@ -903,11 +903,7 @@ unescape_guile (string s) {
   string r;
   for (i=0; i<n; i++) {
     if (s[i] == '\\') {
-      if (i+1 < n && s[i+1] == '\\') {
-        r << "\\\\\\\\";
-        i+=1;
-      }
-      else if (i+3 < n && s[i+1] == 'x'
+      if (i+3 < n && s[i+1] == 'x'
           && is_hex_digit (s[i+2]) && is_hex_digit (s[i+3])) {
         string e= s(i+2, i+4);
         r << (unsigned char) from_hexadecimal (e);