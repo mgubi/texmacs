0787846131cebab43ba62ca4537a7bacf2832f7c
lizabelos <lizabelos@64cb5145-927a-446d-8aed-2fb7b4773692>
2026-01-30 14:39:48 +0000

    add qtkeydisplay
    git-svn-id: svn+ssh://svn.sv.gnu.org/texmacs/trunk@15255 64cb5145-927a-446d-8aed-2fb7b4773692

---
+++ misc/qtkeydisplay/main.cpp
@@ -0,0 +1,63 @@
+#include <QApplication>
+#include <QWidget>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QKeyEvent>
+#include <QKeySequence>
+
+class KeyWidget : public QWidget {
+public:
+    KeyWidget(QWidget *parent = nullptr) : QWidget(parent) {
+        QVBoxLayout *layout = new QVBoxLayout(this);
+        
+        infoLabel = new QLabel("Press any key...", this);
+        infoLabel->setAlignment(Qt::AlignCenter);
+        
+        QFont font = infoLabel->font();
+        font.setPointSize(24);
+        font.setBold(true);
+        infoLabel->setFont(font);
+
+        layout->addWidget(infoLabel);
+        setLayout(layout);
+
+        setWindowTitle("Qt6 Key Event Example");
+        resize(400, 300);
+    }
+
+protected:
+    void keyPressEvent(QKeyEvent *event) override {
+        int key = event->key();
+        Qt::KeyboardModifiers modifiers = event->modifiers();
+
+        bool isModifier = (key == Qt::Key_Control || key == Qt::Key_Shift || 
+                           key == Qt::Key_Alt || key == Qt::Key_Meta);
+
+        QString text;
+        if (isModifier) {
+            text = QKeySequence(key).toString();
+        } else {
+            text = QKeySequence(modifiers | key).toString();
+        }
+
+        if (text.isEmpty()) {
+            text = QString("Unknown Key (%1)").arg(key);
+        }
+
+        infoLabel->setText("Pressed: " + text);
+
+        QWidget::keyPressEvent(event);
+    }
+
+private:
+    QLabel *infoLabel;
+};
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    KeyWidget window;
+    window.show();
+
+    return app.exec();
+}
