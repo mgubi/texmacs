5fe946ad51eadb0b4e3ea2ffed4c0387c392b5d8
lizabelos <lizabelos@64cb5145-927a-446d-8aed-2fb7b4773692>
2026-09-10 13:40:48 +0000

    cmake fix
    git-svn-id: svn+ssh://svn.sv.gnu.org/texmacs/trunk@15717 64cb5145-927a-446d-8aed-2fb7b4773692

--- guile-texmacs/libguile/net_db.c
+++ guile-texmacs/libguile/net_db.c
@@ -458,10 +458,18 @@ scm_init_net_db ()
  scm_no_recovery_key = scm_permanent_object (scm_from_locale_symbol ("no-recovery")) ;
  scm_no_data_key = scm_permanent_object (scm_from_locale_symbol ("no-data")) ;
  scm_c_define_gsubr (s_scm_gethost, 0, 1, 0, (SCM (*)()) scm_gethost); ;
+#if defined (HAVE_GETPROTOENT) || defined (__MINGW32__)
  scm_c_define_gsubr (s_scm_getproto, 0, 1, 0, (SCM (*)()) scm_getproto); ;
+#endif
+#if defined (HAVE_GETSERVENT) || defined (__MINGW32__)
  scm_c_define_gsubr (s_scm_getserv, 0, 2, 0, (SCM (*)()) scm_getserv); ;
+#endif
+#if defined (HAVE_SETPROTOENT) && defined (HAVE_ENDPROTOENT) || defined (__MINGW32__)
  scm_c_define_gsubr (s_scm_setproto, 0, 1, 0, (SCM (*)()) scm_setproto); ;
+#endif
+#if defined (HAVE_SETSERVENT) && defined (HAVE_ENDSERVENT) || defined (__MINGW32__)
  scm_c_define_gsubr (s_scm_setserv, 0, 1, 0, (SCM (*)()) scm_setserv); ;
+#endif
 
 }
 
